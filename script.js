8dff1265e64938d5923284cccb7f54fe5d27b66a
Kappisan <wilkosz.ian@gmail.com>
2016-07-03 00:46:45 +0100

    style changes

--- script.js
+++ script.js
@@ -90,7 +90,9 @@ app.controller('PageCtrl', ['$scope', '$http', '$location', function($s, $http, 
 
 
 	$s.playAgain = function playAgain() {
-
+		$("#game-score").css("display", "none");
+		$(".square").removeClass("p1-square p2-square");
+		$s.move = "p1-place";
 	}
 
 
@@ -230,6 +232,29 @@ app.controller('PageCtrl', ['$scope', '$http', '$location', function($s, $http, 
 	});
 
 
+
+
+	// REMOVE HOVER FOR MOBILE DEVICES
+/*
+	var touch = window.ontouchstart || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+
+	if (touch) { // remove all :hover stylesheets
+	    try { // prevent exception on browsers not supporting DOM styleSheets properly
+	        for (var si in document.styleSheets) {
+	            var styleSheet = document.styleSheets[si];
+	            if (!styleSheet.rules) continue;
+
+	            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+	                if (!styleSheet.rules[ri].selectorText) continue;
+
+	                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+	                    styleSheet.deleteRule(ri);
+	                }
+	            }
+	        }
+	    } catch (ex) {}
+	}
+*/
 
 
 }]);
